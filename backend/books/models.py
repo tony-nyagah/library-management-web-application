be8600f565a427f7b3f79254e9ce6646f38ca456
Antony Nyagah <tony.m.nyagah@gmail.com>
2023-07-23 22:34:25 +0300

    feat: created the book model

---
+++ backend/books/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Books(models.Model):
+    title = models.CharField(max_length=255)
+    synopsis = models.TextField(max_length=500)
+    author = models.CharField(max_length=100)
+    available_copies = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.title
