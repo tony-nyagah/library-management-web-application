25e4ea5a2572d35b0cf75b5233a558d84645ad9f
Antony Nyagah <tony.m.nyagah@gmail.com>
2023-08-09 12:07:34 +0300

    fix: added the catalog model data to the admin site - also added some tests

--- backend/catalog/tests.py
+++ backend/catalog/tests.py
@@ -1,1 +1,26 @@
-# Create your tests here.
+from django.test import TestCase
+
+from catalog.models import Author, Book, Genre, Language
+
+
+class BookModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.test_author = Author.objects.create(first_name="Big", last_name="Bungus")
+        cls.test_genre = Genre.objects.create(name="Fantasy")
+        cls.test_language = Language.objects.create(name="English")
+        cls.test_book = Book.objects.create(
+            title="The Big Book",
+            summary="This is the summary",
+            isbn="123456789",
+            author=cls.test_author,
+            language=cls.test_language,
+        )
+
+    def test_model_content(self):
+        self.assertEqual(self.test_book.title, "The Big Book")
+        self.assertEqual(self.test_book.author, self.test_author)
+        self.assertEqual(self.test_book.language, self.test_language)
+        # self.assertEqual(self.test_book.genre, self.test_genre)
+        self.assertEqual(self.test_book.isbn, "123456789")
+        self.assertEqual(self.test_book.summary, "This is the summary")
