96917dab43f8d2ec39f939ee4a6d2c5cde755a34
Antony Nyagah <tony.m.nyagah@gmail.com>
2023-09-01 16:37:16 +0300

    feat(users/*): setting up a custom user model

--- backend/library_project/urls.py
+++ backend/library_project/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.contrib.auth.models import User
+from django.urls import include, path
+from rest_framework import routers, serializers, viewsets
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api-auth", include("rest_framework.urls")),
     path("api/", include("catalog.urls")),
 ]
