5e6b574571e39da65bba0ac5d0fc490cb3ecc877
Antony Nyagah <tony.m.nyagah@gmail.com>
2023-09-05 17:06:46 +0300

    fix: modified the project urls

--- backend/library_project/urls.py
+++ backend/library_project/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.auth.models import User
 from django.urls import include, path
-from rest_framework import routers, serializers, viewsets
+
+api_urls = [path("catalog/", include("catalog.urls"))]
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth", include("rest_framework.urls")),
-    path("api/", include("catalog.urls")),
+    path("api-v1/", include(api_urls)),
 ]
